f5e3ef5391f77f48a7f11bf818466855b4d401b2
skuttenkuler <samkiddmusic@gmail.com>
2020-01-05 21:15:47 -0800

    axios to github

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const inquirer = require('inquirer');
 const html = require('./html.js');
 const electron = require('electron-html-to');
 
-
+//contstants
+//username
+var username
+//favorite color
+var userColor
+//pdf
+var pdf
 //set up inquirer to get information needed from user
 inquirer.prompt([
     {
@@ -22,5 +28,23 @@ inquirer.prompt([
 ]).then(responses => {
     username = responses.name;
     usercolor = responses.favColor;
-    
+    getProfile();
 })
+
+//get Github data
+//create function
+function getProfile() {
+    //set up query address
+    //perform axios call to get user data
+    var queryData = "https://api.github.com/users/"+username;
+    axios.get(queryData).then(response => {
+        var data = response.data;
+        console.log(data)
+        // then second call to get stars data
+        queryData += "starred";
+        axios.get(queryData).then(starsRes => {
+            var stars = starsRes.data;
+            console.log(stars)
+        }) 
+    })
+}
