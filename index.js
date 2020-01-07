10c492b81f205015be4f1c78a0d59f4f61c57eba
skuttenkuler <samkiddmusic@gmail.com>
2020-01-06 20:22:22 -0800

    update pdf converter, create html template, export data to html.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ inquirer.prompt([
     }
 
 ]).then(responses => {
-    username = responses.name;
-    usercolor = responses.favColor;
+    userName = responses.username;
+    color = responses.favColor;
     getProfile();
 })
 
@@ -36,15 +36,41 @@ inquirer.prompt([
 function getProfile() {
     //set up query address
     //perform axios call to get user data
-    var queryData = "https://api.github.com/users/"+username;
+    var queryData = "http://api.github.com/users/"+ userName;
     axios.get(queryData).then(response => {
         var data = response.data;
-        console.log(data)
+        //console.log(data)
         // then second call to get stars data
-        queryData += "starred";
+        queryData += "/starred";
         axios.get(queryData).then(starsRes => {
-            var stars = starsRes.data;
-            console.log(stars)
+             var stars = starsRes.data;
+             //console.log(stars)
+             dataContent = html.RenderContent(data, stars, color);
+             RenderPdf();
         }) 
     })
+}
+var electronPDF = electron({
+    converterPath: electron.converters.PDF,
+    allowLocalFilesAccess: true
+});
+//function to render a PDF
+function RenderPdf() {
+    console.log("Writing file....");
+    electronPDF({
+        html: pdfContent,
+        pdf: {
+            pageSize: 'Letter',
+            printBackground: true
+        }
+    }, (error, result) => {
+        if(error) {
+            throw error;
+        }
+        fs.createWriteStream(userName + 'pdf');
+        console.log("Profile Generated!");
+        electronPDF.kill();
+    
+    });
+
 }
